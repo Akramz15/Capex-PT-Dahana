2f711db79a2d8b7554e8cbb20c663dc29d41fd84
Akram <naufalakramziyad53@gmail.com>
2026-07-20 14:13:20 +0700

    chore: remove accidental test script files from frontend

--- frontend/patch.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('src/pages/CarryOverPage.jsx', 'utf8');
-content = content.replace(
-  "return (\\n                      <React.Fragment key={kat}>",
-  "console.log('Rendering group:', kat, groupData.length); return (\\n                      <React.Fragment key={kat}>"
-);
-fs.writeFileSync('src/pages/CarryOverPage.jsx', content);
